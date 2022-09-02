0275e6d3fd0697fe51974dfbdbf5a1e15e7b1875
olha0108 <ol0108ha@gmail.com>
2022-09-02 21:11:27 +0300

    08

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,14 +4,19 @@ const form = document.querySelector('.feedback-form');
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 
 const formFields = {
-email: document.querySelector('[name="email"]'),
-message: document.querySelector('[name="message"]'),
-}
+  email: document.querySelector('[name="email"]'),
+  message: document.querySelector('[name="message"]'),
+};
 
 const formData = {};
-if(localStorage.getItem(LOCALSTORAGE_KEY)){
-  const localStorageParseData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-for (key of Object.keys(localStorageParseData)) {formData[key] = localStorageParseData[key]}
+if (localStorage.getItem(LOCALSTORAGE_KEY)) {
+  const localStorageParseData = JSON.parse(
+    localStorage.getItem(LOCALSTORAGE_KEY)
+  );
+  for (key of Object.keys(localStorageParseData)) {
+    formData[key] = localStorageParseData[key];
+    formFields[key].value = localStorageParseData[key];
+  }
 }
 
 form.addEventListener('submit', onSubmitForm);
@@ -27,4 +32,6 @@ function onSubmitForm(evt) {
   evt.preventDefault();
   form.reset();
   localStorage.removeItem(LOCALSTORAGE_KEY);
+  delete formData.email;
+  delete formData.message;
 }
